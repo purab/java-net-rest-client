9b03ed19f3198ef042f290d1f015ed5b7b2a0e69
purab <purabdk@gmail.com>
2021-12-03 22:30:17 -0800

    added object mapper

--- src/main/java/GetUserRestClient.java
+++ src/main/java/GetUserRestClient.java
@@ -1,3 +1,5 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,11 +11,11 @@ public class GetUserRestClient {
     public static void main(String[] args) {
         HttpURLConnection connection = null;
         BufferedReader reader = null;
-        String retVal = null;
+        String json = null;
         try {
             URL resetEndpoint = new URL("https://reqres.in/api/users?page=2");
             connection = (HttpURLConnection) resetEndpoint.openConnection();
-            //Set request method to GET as required from the API
+            // Set request method to GET as required from the API
             connection.setRequestMethod("GET");
 
             // Read the response
@@ -23,25 +25,17 @@ public class GetUserRestClient {
             while ((line = reader.readLine()) != null) {
                 jsonSb.append(line);
             }
-            retVal = jsonSb.toString();
+            json = jsonSb.toString();
 
-            // print out the json response
-            System.out.println(retVal);
-
+            // Converts JSON string to Java object
+            ObjectMapper mapper = new ObjectMapper();
+            // Converts to an array of Book
+            User[] users = mapper.readValue(json, User[].class);
+            for (User user : users) {
+                System.out.println(user);
+            }
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            // Clean up
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (connection != null) {
-                connection.disconnect();
-            }
         }
     }
 }
